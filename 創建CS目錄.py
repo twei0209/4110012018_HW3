defc5c17077ae8b8c5c6bf0dd1a1950cb9124eef
twei0209 <101132185+twei0209@users.noreply.github.com>
2023-09-23 15:53:28 +0800

    Add files via upload

--- 創建CS目錄.py
+++ 創建CS目錄.py
@@ -7,9 +7,4 @@ This is a temporary script file.
 
 import os
 
-cs_directory = "cs"
-
-file_path = os.path.join(cs_directory, "homework.txt")
-
-with open(file_path, "w") as file:
-    file.write("4110012018_鄭婷瑋")
+os.mkdir('CS')
